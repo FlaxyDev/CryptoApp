cf4188cbb4abce2a2981b33bbfbf264451cc59d4
FlaxyDev <flaxyakk@gmail.com>
2025-03-29 18:53:53 +0200

    Add CoinGeckoService for API interaction

--- CryptoApp/obj/Debug/net9.0-windows/CryptoApp.AssemblyInfo.cs
+++ CryptoApp/obj/Debug/net9.0-windows/CryptoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95df488ae8de239a3cf4153b4801485ba0e21fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
