95df488ae8de239a3cf4153b4801485ba0e21fb7
FlaxyDev <flaxyakk@gmail.com>
2025-03-29 18:17:20 +0200

    Create project and add Coin Model

---
+++ CryptoApp/Models/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.Models
+{
+    internal class Coin
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal MarketCapUsd { get; set; }
+        public decimal Volume24hUsd { get; set; }
+        public decimal PercentChange24h { get; set; }
+    }
+}
