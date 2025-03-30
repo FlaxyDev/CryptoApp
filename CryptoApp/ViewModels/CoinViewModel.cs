97d225a4b998f6d8c7b1830e6206d0ef0fe621e2
FlaxyDev <flaxyakk@gmail.com>
2025-03-30 16:17:33 +0300

    added CoinViewModel to manage the list of cryptocurrencies and their updates.

---
+++ CryptoApp/ViewModels/CoinViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using CryptoApp.Models;
+using CryptoApp.Services;
+using System.Threading.Tasks;
+
+namespace CryptoApp.ViewModels
+{
+    public class CoinViewModel
+    {
+        private readonly CoinGeckoService _coinGeckoService;
+
+        public ObservableCollection<Coin> Coins { get; set; }
+
+        public CoinViewModel()
+        {
+            _coinGeckoService = new CoinGeckoService();
+            Coins = new ObservableCollection<Coin>();
+            LoadCoins();
+        }
+
+        private async void LoadCoins()
+        {
+            var coins = await _coinGeckoService.GetTopCoins(10);
+            foreach (var coin in coins)
+            {
+                Coins.Add(coin);
+            }
+        }
+    }
+}
