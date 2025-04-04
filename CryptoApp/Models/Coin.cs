004e7b538d65ecc7d6f0fc33ce6504482295b872
FlaxyDev <flaxyakk@gmail.com>
2025-04-04 16:02:49 +0300

    Update Coin.cs

--- CryptoApp/Models/Coin.cs
+++ CryptoApp/Models/Coin.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CryptoApp.Models
 {
     public class Coin
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("symbol")]
         public string Symbol { get; set; }
+
+        [JsonProperty("current_price")]
         public decimal PriceUsd { get; set; }
+
+        [JsonProperty("market_cap")]
         public decimal MarketCapUsd { get; set; }
+
+        [JsonProperty("total_volume")]
         public decimal Volume24hUsd { get; set; }
+
+        [JsonProperty("price_change_percentage_24h")]
         public decimal PercentChange24h { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; } 
     }
 }
