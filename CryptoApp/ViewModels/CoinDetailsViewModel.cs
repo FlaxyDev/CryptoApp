18edb868efda884c2aa2010fee22bd0691ca3a78
FlaxyDev <flaxyakk@gmail.com>
2025-04-04 16:05:04 +0300

    Create CoinDetailsViewModel.cs

---
+++ CryptoApp/ViewModels/CoinDetailsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoApp.Models;
+
+namespace CryptoApp.ViewModels
+{
+    public class CoinDetailsViewModel
+    {
+        public string Name { get; }
+        public string Symbol { get; }
+        public decimal PriceUsd { get; }
+        public decimal MarketCapUsd { get; }
+        public decimal Volume24hUsd { get; }
+        public decimal PercentChange24h { get; }
+        
+        public string Image { get; }
+        public CoinDetailsViewModel() { }
+        public CoinDetailsViewModel(Coin coin)
+        {
+            Name = coin.Name;
+            Symbol = coin.Symbol;
+            PriceUsd = coin.PriceUsd;   
+            MarketCapUsd = coin.MarketCapUsd;
+            Volume24hUsd = coin.Volume24hUsd;
+            PercentChange24h = coin.PercentChange24h;
+            Image = coin.Image;
+        }
+    }
+}
