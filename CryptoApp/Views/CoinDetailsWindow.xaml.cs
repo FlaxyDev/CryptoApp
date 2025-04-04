0be04c1e005a7f703b9205ba0390a67ce8b80d8f
FlaxyDev <flaxyakk@gmail.com>
2025-04-04 16:08:12 +0300

    Create CoinDetailsWindpw
    A window with detailed information about the cryptocurrency

---
+++ CryptoApp/Views/CoinDetailsWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using CryptoApp.Models;
+using CryptoApp.ViewModels;
+
+namespace CryptoApp.Views
+{
+    public partial class CoinDetailsWindow : Window
+    {
+        public CoinDetailsWindow(Coin coin)
+        {
+            InitializeComponent();
+            DataContext = new CoinDetailsViewModel(coin);
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
